6b6597944bdabbb09ec40a55bd9ada60a185cd18
Evgenii Evseev <ev-ev89@mail.ru>
2023-07-08 17:04:21 +0300

    Верстка about

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+@import url(@/app.scss);
+</style>
